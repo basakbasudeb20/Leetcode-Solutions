f10775cec6c495c1054668d05f81cbee0a6fb353
basakbasudeb20 <68324291+basakbasudeb20@users.noreply.github.com>
2025-03-08 23:39:54 +0530

    Create 242.java

---
+++ 242.java
@@ -0,0 +1,26 @@
+Q. 242. Valid Anagram
+
+S.
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        int[] charCount =new  int[26];
+
+        for(int i = 0 ; i < s.length() ; i++){
+            charCount[s.charAt(i) - 'a']++;
+            charCount[t.charAt(i) - 'a']--;
+        }
+
+
+        for(int i : charCount){
+            if(i != 0 ){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+}
